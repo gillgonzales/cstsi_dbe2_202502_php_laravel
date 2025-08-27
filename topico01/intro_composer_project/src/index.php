595cd2f0e424cc7aa2445a4cd0391e63a61fb45c
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-08-27 20:08:56 -0300

    refactor: configurando o diretório para o load do .env

--- topico01/intro_composer_project/src/index.php
+++ topico01/intro_composer_project/src/index.php
@@ -1,7 +1,9 @@
 <?php
 require "../vendor/autoload.php";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// echo __DIR__;
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 // var_dump($dotenv);
