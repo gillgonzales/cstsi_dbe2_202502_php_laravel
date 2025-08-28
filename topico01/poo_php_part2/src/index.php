1f72c5b4a8e51b0bbe332e7491f18d47b66d64f2
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-08-27 21:11:29 -0300

    feat: exemplo dependencia illuminate/support. Uso de novos namespaces para importação da classe Str e uso do metodo slug,  composer require illuminate/support

--- topico01/poo_php_part2/src/index.php
+++ topico01/poo_php_part2/src/index.php
@@ -5,10 +5,12 @@ require "../vendor/autoload.php";
 use Gvg\Dbe2\classes\Atleta;
 
 
-$atl1 = new Atleta("Luizito",36,1.8,80);
+$atl1 = new Atleta("Luizito Soares",36,1.8,80);
 
 echo "<hr>";//---
 echo "<pre>";
 $atl1->showImc();
 
 var_dump($atl1);
+
+require "slugfy.php";
