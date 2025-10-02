b287bb8321c503acb1c82bf3e2beac79acc4077b
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-10-01 22:51:36 -0300

    feat: criação da primeira rota de exemplo ola

--- topico02/routes/web.php
+++ topico02/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('ola',function(){
+    echo "Hola Mundo !!!";
 });
