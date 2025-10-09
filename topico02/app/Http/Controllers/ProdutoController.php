6a298f7ba1b2c735dfb0fc9b57639e881a528451
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-10-08 22:52:50 -0300

    feat: adicionando metodos do crud no controller de produto Métodos de create, store e edit funcionais.

--- topico02/app/Http/Controllers/ProdutoController.php
+++ topico02/app/Http/Controllers/ProdutoController.php
@@ -4,14 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class ProdutoController extends Controller
 {
     public function index() {
         $listProdutos = Produto::all();
+        // dd($listProdutos);
         // return response()->json($listProdutos);
-        return view('produtos.index',["listProdutos"=>$listProdutos]);
+
+        return View::make('produtos.index',["listProdutos"=>$listProdutos]);
     }
 
-    public function show($id){}
+    public function show($id){
+        $produto = Produto::find($id);
+        // dd($produto);
+        return view('produtos.show',['produto'=>$produto]);
+    }
+
+    public function create(){
+        return view('produtos.create');
+    }
+
+    public function store(Request $request){
+        // dd($request->all());
+        $novoProduto = $request->all();
+        $novoProduto['importado'] = $request->has('importado');
+        if(Produto::create($novoProduto))
+            return redirect('/produtos');
+        else dd("Erro ao inserir produto!!!");
+    }
+
+    public function edit($id){
+        $produto = Produto::find($id);
+        if($produto)
+            return view('produtos.edit',compact('produto'));
+        else dd("Produto não encontrado!!!");
+    }
+
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function delete($id){
+
+
+    }
+
+    public function remove($id){
+
+    }
 }
