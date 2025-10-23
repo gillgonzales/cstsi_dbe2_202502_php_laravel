59c5def72c12e3a43e7e87dea2456516ef5e8482
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-10-23 18:05:27 -0300

    feat: criação das rotas da API com apiResources. Criação dos endpoints de 'produtos' para a api com apiResource. sail/php artisan route:list --path=api/produtos

--- topico02/routes/api.php
+++ topico02/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Rotas da api para o endpoint produtos com apiResource
+Route::apiResource('produtos',ProdutoController::class);
