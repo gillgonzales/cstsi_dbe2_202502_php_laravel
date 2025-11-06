dd901ed6d61d7376cdbe922b4abbcca20d29b999
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-11-05 22:45:39 -0300

    feat: criação de factory e seeder para test. sail  artisan make:factory ProdutoFactory -m Produto.

--- topico02/database/seeders/DatabaseSeeder.php
+++ topico02/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Produto;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        
+        Produto::factory(20)->create();
+
     }
 }
