fde7480cda6e554c66f576dd76b12e0e664ad8e6
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-11-20 09:45:55 -0300

    refactor: configuração da model de Fornecedor Nesta model, por sair do padrão de nomes do Active Records, configuramos o nome da tabela ($table). Também adicionamos os relacionamentos com Estado (belongsTo) ne Produtos (hasMany).

--- topico02/app/Models/Fornecedor.php
+++ topico02/app/Models/Fornecedor.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'fornecedores';
+    protected $fillable = [
+        "nome",
+        "cnpj",
+        "email",
+        "estado_id",
+        "telefone",
+        "endereco"
+    ];
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class);
+    }
+
+    public function produtos() {
+        return $this->hasMany(Produto::class);
+    }
 }
