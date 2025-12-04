4153a5e6e885662ee4444a9f8ae4b254177edb17
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-12-04 11:40:32 -0300

    refactor: aplicando transação com DB:transaction. Neste caso, se algum dos processos dentro da closure passada como argumento para a transação falhar, será executado um rollback no banco de dados, mantendo o estado original.

--- topico02/app/Repositories/ProdutoRepository.php
+++ topico02/app/Repositories/ProdutoRepository.php
@@ -5,33 +5,41 @@ namespace App\Repositories;
 use App\Models\Produto;
 use App\Services\ProdutoUploadService;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 class ProdutoRepository
 {
     public static function store(array $produtoData): Produto
     {
-        if (!isset($produtoData['imagem']) && !isset($produtoData['video']))
-            return Produto::create($produtoData);
+        try {
+            if (!isset($produtoData['imagem']) && !isset($produtoData['video']))
+                return Produto::create($produtoData);
 
+            $novoProduto = Produto::make($produtoData);
 
-        $novoProduto = Produto::create($produtoData);
+            DB::transaction(function () use ($produtoData, $novoProduto) {
+                $novoProduto->save();
+                if (isset($produtoData['imagem'])) {
+                    $produtoData['imagem'] = ProdutoUploadService::handleUploadFile($produtoData['imagem']);
+                    if (!$produtoData['imagem'])
+                        throw new Exception("Erro ao salvar produto com imagem!!");
 
-        if (isset($produtoData['imagem'])) {
-            $produtoData['imagem'] = ProdutoUploadService::handleUploadFile($produtoData['imagem']);
-            if (!$produtoData['imagem'])
-                throw new Exception("Erro ao salvar produto com imagem!!");
+                     $novoProduto->media()->create([
+                        'source' => $produtoData['imagem']
+                    ]);
+                }
 
-            $novoProduto->media()->create([
-                'source' => $produtoData['imagem']
-            ]);
+                if (isset($produtoData['video'])) {
+                    $novoProduto->media()->create([
+                        'source' => $produtoData['videos']
+                    ]);
+                }
+                $novoProduto->load('media');
+                $novoProduto->refresh();
+            });
+            return $novoProduto;
+        } catch (Exception $error) {
+            throw $error;
         }
-
-        if (isset($produtoData['video'])) {
-            $novoProduto->media()->create([
-                'source' => $produtoData['videvideoo']
-            ]);
-        }
-
-        return $novoProduto->load('media');
     }
 }
