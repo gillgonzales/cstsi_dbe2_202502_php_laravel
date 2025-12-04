24beda154e27c042bad5d1dfb98b4e0320e3598d
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-12-03 21:39:15 -0300

    feat: produto resource para modificar o campo media

--- topico02/app/Http/Resources/ProdutoResource.php
+++ topico02/app/Http/Resources/ProdutoResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProdutoResource extends JsonResource
 {
@@ -14,6 +16,17 @@ class ProdutoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return
+        [
+            ...parent::toArray($request),
+            'media'=>$this->whenLoaded('media',function(){
+                // return $this->media->map(fn($m)=>$m->source);
+                return  $this->media->map(function($media){
+                   if(Str::contains($media->source, 'http'))
+                        return $media->source;
+                    else return asset(Storage::url('produtos/'.$media->source));
+                });
+            }),
+        ];
     }
 }
