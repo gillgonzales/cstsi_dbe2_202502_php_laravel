41ef207c2a97fc901f2bbbeb0c376072d19df966
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-12-17 20:21:58 -0300

    feat: adiciona config de ratelimiting na api.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    //Para limitar requisições na API por usuário ou IP
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
