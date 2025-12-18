cf8f853dd2ebe017728931306933ffe168418f36
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-12-17 21:20:33 -0300

    feat: adiciona o campo public_id para o cloudinary

--- database/migrations/2025_11_25_011640_create_produtos_table.php
+++ database/migrations/2025_11_25_011640_create_produtos_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->float('preco');
             $table->integer('qtd_estoque');
             $table->boolean('importado')->default(false);
+            $table->string('public_id')->nullable();
             $table->timestamps();
         });
     }
